4aa6deff034c6bba21bad5a7067706e613940aff
lukasz <lukaszuczkiewicz@gmail.com>
2020-01-01 22:03:05 +0100

    add queue data structure

---
+++ scripts/queue.ts
@@ -0,0 +1,43 @@
+export class Node {
+  value: any;
+  next: any;
+  
+  constructor(value: any){
+      this.value = value;
+      this.next = null;
+  }
+}
+
+export class Queue {
+  first: any;
+  last: any;
+  size: number;
+  constructor(){
+      this.first = null;
+      this.last = null;
+      this.size = 0;
+  }
+  enqueue(val: any): number{
+      var newNode = new Node(val);
+      if(!this.first){
+          this.first = newNode;
+          this.last = newNode;
+      } else {
+          this.last.next = newNode;
+          this.last = newNode;
+      }
+      return ++this.size;
+  }
+
+  dequeue(): any {
+      if(!this.first) return null;
+
+      var temp = this.first;
+      if(this.first === this.last) {
+          this.last = null;
+      }
+      this.first = this.first.next;
+      this.size--;
+      return temp.value;
+  }
+}
